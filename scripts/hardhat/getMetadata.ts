353a85e768cde3b5bb44354097f0bf9b9ae36e77
Carter Carlson <pegahcarter@gmail.com>
2024-08-15 14:44:48 -0700

    chore: extract metadata

---
+++ scripts/hardhat/getMetadata.ts
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2024 Circle Internet Financial, LTD. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from "fs";
+
+function main() {
+  const buildInfo = JSON.parse(
+    fs.readFileSync(
+      "./artifacts/hardhat/build-info/a581d1aa6c8ba09202e5526b98d24c3d.json",
+      "utf-8"
+    )
+  );
+
+  // FiatTokenProxy
+  let contractName = "FiatTokenProxy";
+  let metadata =
+    buildInfo.output.contracts[`contracts/v1/${contractName}.sol`][contractName]
+      .metadata;
+  fs.writeFileSync(`metadata/${contractName}.json`, metadata, {
+    flag: "w",
+  });
+
+  // FiatTokenV2_2
+  contractName = "FiatTokenV2_2";
+  metadata =
+    buildInfo.output.contracts[`contracts/v2/${contractName}.sol`][contractName]
+      .metadata;
+  fs.writeFileSync(`metadata/${contractName}.json`, metadata, {
+    flag: "w",
+  });
+
+  // SignatureChecker
+  contractName = "SignatureChecker";
+  metadata =
+    buildInfo.output.contracts[`contracts/util/${contractName}.sol`][
+      contractName
+    ].metadata;
+  fs.writeFileSync(`metadata/${contractName}.json`, metadata, {
+    flag: "w",
+  });
+}
+
+main();
